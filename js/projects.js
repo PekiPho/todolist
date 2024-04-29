b1d63fc655b29188bb8f0e23488954d907a845f7
PekiPho <v.a.r.d.games123@gmail.com>
2024-04-29 15:15:42 +0200

    Added append to projects

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,10 @@
 export default class Projects{
     constructor(name) {
         this.name=name;
+        let left=document.querySelector(".left");
+        let proj=document.createElement("p");
+        proj.textContent=this.name;
+        left.appendChild(proj);
     }
     
     remove(){
--- main.js
+++ main.js
@@ -4,11 +4,31 @@ import Project from './js/projects.js';
 let proj = document.querySelector(".new");
 let modalproj=document.querySelector(".modal");
 let overlayproj=document.querySelector(".overlay");
+let exitproj=document.querySelector(".modal>.exit");
+let projsumbit=document.querySelector(".modal>button");
 
 let p= new Array(5);
+let i=0;
 
 proj.addEventListener('click',()=>{
     modalproj.classList.add("active");
     overlayproj.classList.add("active");
 });
 
+exitproj.addEventListener('click',()=>{
+    modalproj.classList.remove("active");
+    overlayproj.classList.remove("active");
+});
+
+projsumbit.addEventListener('click',()=>{
+    if(i>4){
+        alert("Too many projects");
+        return;
+    }
+    let input=document.querySelector('.title_pr');
+    p[i]=new Project(input.value);
+    input.value="";
+    i++;
+    modalproj.classList.remove("active");
+    overlayproj.classList.remove("active");
+});
