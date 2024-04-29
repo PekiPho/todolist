04c2e8d5646a87bca94cb669543ce2ef662b1c62
PekiPho <v.a.r.d.games123@gmail.com>
2024-04-29 15:34:36 +0200

    styled some more

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,19 @@
+import toDo from "./todo.js";
+
 export default class Projects{
     constructor(name) {
         this.name=name;
         let left=document.querySelector(".left");
         let proj=document.createElement("p");
+        proj.classList.add("proj");
         proj.textContent=this.name;
         left.appendChild(proj);
     }
     
+    addTask(){
+        
+    }
+
     remove(){
 
     }
